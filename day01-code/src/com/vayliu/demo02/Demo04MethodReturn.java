733201c475d4e49906d9ee1d574404cf0e8d49dd
VayLiu <935029352@qq.com>
2020-08-01 23:44:45 +0800

    题目变形

--- day01-code/src/com/vayliu/demo02/Demo04MethodReturn.java
+++ day01-code/src/com/vayliu/demo02/Demo04MethodReturn.java
@@ -1,8 +1,11 @@
 package com.vayliu.demo02;
 
 /*
-题目要求：定义一个方法，求出两个数字之和。
+题目要求：定义一个方法，【求出】两个数字之和。
 题目解析：你帮我算，算完把结果告诉我。
+
+题目变形：定义一个方法，【打印】两个数字之和。
+题目解析：你帮我算，算完后你自己负责显示结果，不需要告诉我。
  */
 public class Demo04MethodReturn {
 
@@ -11,6 +14,9 @@ public class Demo04MethodReturn {
         //我调用你，你帮我计算一下，算完之后把结果告诉我的num变量
         int num = getSum(10,20);
         System.out.println("返回值是：" + num);
+        System.out.println("===========");
+
+        printSum(10,25);
     }
 
     //我是一个方法，我负责两个数字相加。
@@ -18,5 +24,12 @@ public class Demo04MethodReturn {
     public static int getSum(int a, int b) {
         int result = a + b;
         return result;
+    }
+
+    //我是一个方法，我负责两个数字相加。
+    //我没有返回值，不会把结果告诉任何人，而是我自己进行打印输出。
+    public static void printSum(int a, int b) {
+        int result = a + b;
+        System.out.println("结果是：" + result);
     }
 }
