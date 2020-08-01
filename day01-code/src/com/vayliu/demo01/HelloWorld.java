c328849f12a31fa1d22478e7ed5944e40c77e9d4
VayLiu <935029352@qq.com>
2020-08-01 22:39:12 +0800

    打印输出HelloWorld

---
+++ day01-code/src/com/vayliu/demo01/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.vayliu.demo01;
+
+/**
+ * 打印输出 HelloWorld！
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!");
+    }
+}
