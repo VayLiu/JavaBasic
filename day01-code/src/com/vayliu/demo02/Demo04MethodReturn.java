7e14669364d418ed0e0c9878f27b5c336cee8403
VayLiu <935029352@qq.com>
2020-08-01 23:56:25 +0800

    方法返回值注意事项

--- day01-code/src/com/vayliu/demo02/Demo04MethodReturn.java
+++ day01-code/src/com/vayliu/demo02/Demo04MethodReturn.java
@@ -6,17 +6,34 @@ package com.vayliu.demo02;
 
 题目变形：定义一个方法，【打印】两个数字之和。
 题目解析：你帮我算，算完后你自己负责显示结果，不需要告诉我。
+
+注意事项：
+对于有返回值的方法，可以使用单独调用、打印调用或赋值调用。
+对于无返回值的方法，只能使用单独调用，不能使用打印调用或赋值调用。
  */
 public class Demo04MethodReturn {
 
     public static void main(String[] args) {
         //我是main方法，我来调用你。
         //我调用你，你帮我计算一下，算完之后把结果告诉我的num变量
-        int num = getSum(10,20);
+        int num = getSum(10,20);//赋值调用——正确写法
         System.out.println("返回值是：" + num);
         System.out.println("===========");
 
         printSum(10,25);
+        System.out.println("===========");
+
+        System.out.println(getSum(2,3));//输出调用——正确写法
+        getSum(3,5);//单独调用——正确写法，但是返回值没有用到
+        System.out.println("===========");
+
+        //对于void类型（没有返回值）的方法，只能单独调用，不能打印或者赋值
+//        System.out.println(printSum(2,3));//打印调用——错误写法！
+//        System.out.println(void);//就相当于打印void
+
+//        int num2 = printSum(5,6);//赋值调用——错误写法！
+//        int num3 = void;//就相当于赋值void
+
     }
 
     //我是一个方法，我负责两个数字相加。
