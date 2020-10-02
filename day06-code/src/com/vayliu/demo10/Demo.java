09a5b96ab3dd615ea6e8de6f4370832789d9917e
VayLiu <935029352@qq.com>
2020-10-02 22:42:01 +0800

    继承在内存中的调用

---
+++ day06-code/src/com/vayliu/demo10/Demo.java
@@ -0,0 +1,14 @@
+package com.vayliu.demo10;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        Zi zi = new Zi();
+
+        zi.show();
+        zi.method();
+
+    }
+
+}
